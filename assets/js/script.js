434381adc489e9e8b3a74f633e55ad8ceb83d538
goose <anrobbins18@gmail.com>
2023-04-20 21:15:57 -0400

    added script logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 //TODO your main logic here
+let searchEl = document.querySelector("#search-form");
+let formatEl = document.querySelector("#format-input")
+let submitBtn = document.querySelector(".btn-info")
+
+
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault()
+    var searchValue = searchEl.textContent
+    var formatValue = formatEl.textContent
+    localStorage.setItem("search-value", searchValue);
+    localStorage.setItem("format-value", formatValue);
+    window.location.href = "./index.html"
+})
